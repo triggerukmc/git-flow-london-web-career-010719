4d4c3150e735a89191970fcee31f42c2dfae28d0
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-03 13:19:52 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,9 +17,9 @@ def encode(string)
   Digest::SHA1.hexdigest(string)
 end
 
-def read_file(file_name)
-  file = File.open(file_name, "r")
-  data = file.read
-  file.close
-  return data
-end
+#def read_file(file_name)
+ # file = File.open(file_name, "r")
+  #data = file.read
+  #file.close
+  #return data
+#end
